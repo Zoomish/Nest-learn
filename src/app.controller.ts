ee1e1d1005547a2617720b15b31b09418560850c
Zoom <egor147536987@gmail.com>
2024-02-12 18:09:51 +0300

    (generated by blackbox.ai )

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
+import { AppService } from './app.service';
 
 import { Controller, Get } from '@nestjs/common';
 @Controller('/api')
 
 export class AppController {
+
+    constructor(private appService: AppService){}
+
     @Get('/users')
     getUsers() {
-        return [{ id: 1, name: 'Zoomish' }];
+        return this.appService.getUsers()
     }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
 
 @Module({
     controllers: [AppController],
     providers: [AppService],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'root',
+            database: 'nest',
+            models: [],
+            autoLoadModels: true,
+            synchronize: true,
+        }),
+    ],
 })
 
 export class AppModule { }
