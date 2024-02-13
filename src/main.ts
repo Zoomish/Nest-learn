aec6f74bb801a8a1039dec93f7779102ffce4389
Zoom <egor147536987@gmail.com>
2024-02-13 17:15:29 +0300

    Update database connection details to use environment variables. (generated by blackbox.ai )

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function start() {
     const app = await NestFactory.create(AppModule);
-    const PORT = process.env.PORT || 5000;
+    const PORT = Number(process.env.PORT) || 5000;
 
     await app.listen(PORT, () => console.log(`Server run on ${PORT}`));
 }
