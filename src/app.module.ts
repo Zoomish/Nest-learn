101ecb1eef14cb452133f2b3ae105c4e64e932f5
Zoom <egor147536987@gmail.com>
2024-02-13 16:29:03 +0300

    Update dependencies and remove unused files. (generated by blackbox.ai )

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,27 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
             host: 'localhost',
             port: 5432,
             username: 'postgres',
             password: 'root',
-            database: 'nest',
+            database: 'nest-learn',
             models: [],
             autoLoadModels: true,
             synchronize: true,
         }),
+        UsersModule,
     ],
 })
 
--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-@Injectable()
-
-export class AppService {
-    getUsers(){
-        return [{ id: 1, name: 'Zoomish' }];
-    }
-}
