37093ca69cf3a782808816c5eae8d5ec480befcd
Zoom <egor147536987@gmail.com>
2024-02-13 16:55:20 +0300

    Update dependencies and add dotenv configuration (generated by blackbox.ai )

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from "@nestjs/config";
-
 @Module({
     controllers: [],
     providers: [],
     imports: [
         ConfigModule.forRoot({
             envFilePath: '.env',
+            isGlobal: true,
         }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from './app.module';
-
+require('dotenv').config()
 async function start() {
-    const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+    console.log(process.env.PORT)
+    const PORT = parseInt(process.env.PORT, 10) || 5000;
 
     await app.listen(PORT, () => console.log(`Server run on ${PORT}`));
 }
