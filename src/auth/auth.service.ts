6030d7fe2cab97641e751cfffd89a284ebba183f
Zoom <egor147536987@gmail.com>
2024-02-14 20:56:08 +0300

    Add bcrypt for password hashing and check if user already exists. (generated by blackbox.ai )

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
-
+import * as bcrypt from 'bcryptjs';
 @Injectable()
 export class AuthService {
 
@@ -14,5 +14,10 @@ export class AuthService {
 
     async registration(userDto: CreateUserDto) {
         const candidate = await this.userService.getUserByEmail(userDto.email);
+        if (candidate) {
+            throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+        }
+        const hashPassword = await bcrypt.hash(userDto.password, 10);
+        const user = await this.userService.createUser({...userDto, password: hashPassword});
     }
 }
