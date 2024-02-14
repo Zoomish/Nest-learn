b70c7746e042f1fa1bcdcace6bd1d1196af1ffef
Zoom <egor147536987@gmail.com>
2024-02-14 14:05:00 +0300

    Add JWT authentication. (generated by blackbox.ai )

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import {User} from "./users/users.model";
 import { RolesModule } from './roles/roles.module';
 import {Role} from "./roles/roles.model";
 import {UserRoles} from "./roles/user-roles.model";
+import { AuthModule } from './auth/auth.module';
 @Module({
     controllers: [],
     providers: [],
@@ -27,6 +28,7 @@ import {UserRoles} from "./roles/user-roles.model";
         }),
         UsersModule,
         RolesModule,
+        AuthModule,
     ],
 })
 
