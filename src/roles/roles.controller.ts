6424b9eae30555dbcc61f46c8705b96335f8c2a6
Zoom <egor147536987@gmail.com>
2024-02-14 14:10:04 +0300

    Add Swagger API documentation for the Roles controller (generated by blackbox.ai )

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,11 +1,16 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Role } from './roles.model';
 
+@ApiTags('Роли')
 @Controller('roles')
 export class RolesController {
     constructor(private roleService: RolesService) { }
 
+    @ApiOperation({ summary: 'Создание роли' })
+    @ApiResponse({ status: 200, type: Role })
     @Post()
     create(@Body() dto: CreateRoleDto) {
         return this.roleService.createRole(dto);
