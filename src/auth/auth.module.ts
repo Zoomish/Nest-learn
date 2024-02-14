4007e9ab096e2bafcd9d738366ca90ccd4604504
Zoom <egor147536987@gmail.com>
2024-02-14 22:34:13 +0300

    Changed imports to be forwardRef in auth.module.ts and users.module.ts. (generated by blackbox.ai )

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
@@ -8,7 +8,7 @@ import { JwtModule } from '@nestjs/jwt';
   providers: [AuthService],
   controllers: [AuthController],
   imports: [
-    UsersModule,
+    forwardRef(() => UsersModule),
     JwtModule.register({
       secret: process.env.PRIVATE_KEY || 'SECRET',
       signOptions: {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,18 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './users.model';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Role } from 'src/roles/roles.model';
 import { RolesModule } from 'src/roles/roles.module';
+import { AuthService } from 'src/auth/auth.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
     controllers: [UsersController],
     providers: [UsersService],
     imports: [
         SequelizeModule.forFeature([User, Role]),
-        RolesModule
+        RolesModule,
+        forwardRef(()=>AuthModule),
     ],
-    exports: [UsersService]
+    exports: [
+        UsersService
+    ]
 })
 export class UsersModule { }
