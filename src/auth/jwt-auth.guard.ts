9558cedfcfd5fed9d4c26522981022d66026c306
Zoom <egor147536987@gmail.com>
2024-02-14 22:31:06 +0300

    Add JWT authentication guard (generated by blackbox.ai )

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,9 +1,25 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
 
-@Injectable()
 export class Guard implements CanActivate {
+
+    constructor(private jwtService: JwtService) { }
+
     canActivate(context: ExecutionContext,): boolean | Promise<boolean> | Observable<boolean> {
-        return undefined
+        const req = context.switchToHttp().getRequest();
+        try {
+            const authHeader = req.headers.authorization;
+            const bearer = authHeader.split(' ')[0];
+            const token = authHeader.split(' ')[1];
+            if (!bearer.includes('Bearer') || !token) {
+                throw new UnauthorizedException({ message: 'Пользователь не авторизован' })
+            }
+            const user = this.jwtService.verify(token);
+            req.user = user;
+            return true;
+        } catch (error) {
+            throw new UnauthorizedException({ message: 'Пользователь не авторизован' })
+        }
     }
 }
