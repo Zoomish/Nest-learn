50b249612c1175f9ccefc323feaac1f5269d0a18
Zoom <egor147536987@gmail.com>
2024-02-15 17:48:40 +0300

    Ban user if exists, throw error if not (generated by blackbox.ai )

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -42,6 +42,9 @@ export class UsersService {
 
     async ban(dto: BanUserDto) {
         const user = await this.userRepository.findByPk(dto.userId);
+        if (!user) {
+            throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+        }
         user.banned = true;
         user.banReason = dto.banReason;
         await user.save();
