bb24a6a7bff7b0be0b66c73fc8672412aaf34e58
Zoom <egor147536987@gmail.com>
2024-02-16 15:17:32 +0300

    Added validation pipe to the NestJS application. (generated by blackbox.ai )

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function start() {
     const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function start() {
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('/api/docs', app, document);
+    
+    app.useGlobalPipes( new ValidationPipe());
 
     await app.listen(PORT, () => console.log(`Server run on ${PORT}`));
 }
