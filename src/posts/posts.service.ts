6d115317afddf2820fa6d3b4d6aa36b9886b565e
Zoom <egor147536987@gmail.com>
2024-02-16 15:49:33 +0300

    Add `Post` model and `create` method to `PostsService`. (generated by blackbox.ai )

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Post } from './posts.model';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+
+    constructor(@InjectModel(Post) private userRepository: typeof Post) { }
+
+    async create(dto: CreatePostDto, image) {
+        const post = await this.userRepository.create(dto);
+        return post;
+    }
+}
