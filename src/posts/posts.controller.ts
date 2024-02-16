161ad4a4de86cf81c128b704c1d79c99b0e4e546
Zoom <egor147536987@gmail.com>
2024-02-16 15:47:27 +0300

    Added `@UploadedFile` decorator to the `Post` route in `posts.controller.ts`. (generated by blackbox.ai )

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,6 @@
 import { CreatePostDto } from './dto/create-post.dto';
 import {PostsService} from './posts.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
