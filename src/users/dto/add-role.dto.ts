dcc51f99bb0fb1858289e77fdc44f8633551b78b
Zoom <egor147536987@gmail.com>
2024-02-16 15:19:07 +0300

    Add type annotations to AddRoleDto properties (generated by blackbox.ai )

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,5 +1,8 @@
+import { IsNumber, IsString } from "class-validator"
 
 export class AddRoleDto {
+    @IsString({ message: 'Должно быть строкой' })
     readonly value: string
+    @IsNumber({ allowNaN: false, allowInfinity: false }, { message: 'Должно быть числом' })
     readonly userId: number
 }
