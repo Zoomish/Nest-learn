6699f86392033be2b3c789e26963b7848928a8a3
Zoom <egor147536987@gmail.com>
2024-02-16 15:12:33 +0300

    Add validation to user creation (generated by blackbox.ai )

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -1,9 +1,22 @@
 import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { ValidationException } from 'src/exceptions/validation.exception';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
-    transform(value: any, metadata: ArgumentMetadata) {
+    async transform(value: any, metadata: ArgumentMetadata) {
         const obj = plainToClass(metadata.metatype, value);
+        const errors = await validate(obj)
+
+        if (errors.length) {
+            let messages = errors.map(err => {
+                return {
+                    [err.property]: Object.values(err.constraints).join(', ')
+                }
+            })
+            throw new ValidationException(messages)
+        }
+        return value
     }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -16,6 +16,7 @@ export class UsersController {
 
     @ApiOperation({ summary: 'Создание пользователя' })
     @ApiResponse({ status: 200, type: User })
+    @UsePipes(ValidationPipe)
     @Post()
     create(@Body() userDto: CreateUserDto) {
         return this.userService.createUser(userDto);
