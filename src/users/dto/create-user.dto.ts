35a98965a230b702ec8dfd242aadbb1aeffc1994
Zoom <egor147536987@gmail.com>
2024-02-16 15:02:40 +0300

    Add email and password validation to CreateUserDto (generated by blackbox.ai )

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString, Length } from "class-validator";
 
 
 export class CreateUserDto {
     @ApiProperty({ example: 'test@gmail.com', description: 'Почтовый адрес' })
+    @IsString({ message: 'Должно быть строкой' })
     readonly email: string;
 
     @ApiProperty({ example: '12345', description: 'Пароль пользователя' })
+    @IsString({ message: 'Должно быть строкой' })
+    @Length(4, 16, { message: 'От 4 до 16 символов' })
     readonly password: string;
 }
