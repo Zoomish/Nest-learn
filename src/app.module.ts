51f528bc94b1f1caa7b00f77b7d9844b3304ab91
Zoom <egor147536987@gmail.com>
2024-02-16 15:21:28 +0300

    Add PostsModule to the list of imported modules. (generated by blackbox.ai )

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { RolesModule } from './roles/roles.module';
 import {Role} from "./roles/roles.model";
 import {UserRoles} from "./roles/user-roles.model";
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 @Module({
     controllers: [],
     providers: [],
@@ -29,6 +30,7 @@ import { AuthModule } from './auth/auth.module';
         UsersModule,
         RolesModule,
         AuthModule,
+        PostsModule,
     ],
 })
 
