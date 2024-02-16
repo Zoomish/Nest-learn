4306a46279a627a856cf0074ed7a3798b275abd0
Zoom <egor147536987@gmail.com>
2024-02-16 15:58:55 +0300

    Added file upload functionality to the posts service. (generated by blackbox.ai )

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class FilesService {
 
-    async createFile(file) {
+    async createFile(file): Promise<string> {
         return 'This action adds a new file';
     }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Post } from './posts.model';
 import { CreatePostDto } from './dto/create-post.dto';
+import { FilesService } from 'src/files/files.service';
 
 @Injectable()
 export class PostsService {
 
-    constructor(@InjectModel(Post) private postRepository: typeof Post) { }
+    constructor(@InjectModel(Post) private postRepository: typeof Post,
+    private fileService: FilesService) { }
 
     async create(dto: CreatePostDto, image: any) {
-        const fileName = 'asfas'
+        const fileName = this.fileService.createFile(image)
         const post = await this.postRepository.create({...dto, image: fileName});
         return post;
     }
