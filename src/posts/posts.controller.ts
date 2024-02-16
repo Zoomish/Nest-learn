0a27175af21395cc96b854bbcce68fb4576e5075
Zoom <egor147536987@gmail.com>
2024-02-16 15:43:24 +0300

    Added `PostsService` to the `PostsController` constructor and added a `createPost` method. (generated by blackbox.ai )

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import {PostsService} from './posts.service';
 
 @Controller('posts')
 export class PostsController {
 
+    constructor(private postsService: PostsService) { }
+    
+    @Post()
     createPost(dto: CreatePostDto){
 
     }
