672b3135f9cc6e4b3b33350f32b784481c6f2dfb
Zoom <egor147536987@gmail.com>
2024-02-16 15:35:54 +0300

    Add UserRoles and Post models to the Users module. (generated by blackbox.ai )

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,12 +7,14 @@ import { Role } from 'src/roles/roles.model';
 import { RolesModule } from 'src/roles/roles.module';
 import { AuthService } from 'src/auth/auth.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { UserRoles } from 'src/roles/user-roles.model';
+import { Post } from 'src/posts/posts.model';
 
 @Module({
     controllers: [UsersController],
     providers: [UsersService],
     imports: [
-        SequelizeModule.forFeature([User, Role]),
+        SequelizeModule.forFeature([User, Role, UserRoles, Post]),
         RolesModule,
         forwardRef(()=>AuthModule),
     ],
